db463dc6199ae24647780fa7b64bf346d22c9172
vieiraailton-dev <vieira.ailton@escola.pr.gov.br>
2026-06-25 12:02:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function revelarMensagem() {
+  document.getElementById("mensagem").innerText = "ola, você alterou a mensagem chamando uma função JS"
+}
+function mostrarNome() {
+  let nome = document.getElementById("mensagem")
+}
