a24dc5e7f8aa58409da4d134650719f44e51266b
vieiraailton-dev <vieira.ailton@escola.pr.gov.br>
2026-06-26 08:03:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ function revelarMensagem() {
   document.getElementById("mensagem").innerText = "ola, você alterou a mensagem chamando uma função JS"
 }
 function mostrarNome() {
-  let nome = document.getElementById("mensagem")
-}
+  let nome = document.getElementById("mensagem").value;
+ document.getElementById("resultadoNome").innerText = "Bem vindo, "+ nome +"!";
+} 
